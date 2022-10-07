8a40325052c866f0bae70dd2a7d18b66ad95d380
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-10-07 19:44:23 -0400

    Update user.js
    *USER.JS*
    
    - Added thoughts, friends and friendcount virtual

--- models/user.js
+++ models/user.js
@@ -1,35 +1,50 @@
 const { Schema, Types } = require("mongoose");
 
-const userSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  userName: {
-    type: String,
-    unique: true,
-    required: [true, "You MUST provide a username."],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-      "Please enter a valid email address",
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    userName: {
+      type: String,
+      unique: true,
+      required: [true, "You MUST provide a username."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
     ],
   },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "thought",
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  ],
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return friends.length;
 });
+
+const User = model("user", userSchema);
+
+module.exports = User;
